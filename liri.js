058e277d2cfe6941f2e5eafd5f9ed8d94f4566e4
alex nichols <alex nichols>
2017-03-27 21:51:53 -0700

    js update

--- liri.js
+++ liri.js
@@ -1,5 +1,13 @@
+//dont forget to require spotify and omdb!!!
+
+
 var keysExport = require("./keys.js");
 
 var keysData = keysExport.twitterKeys;
 
+var twitter = require("twitter");
+
+var commandArgs = process.argv[2];
+
 console.log(keysData);
+
